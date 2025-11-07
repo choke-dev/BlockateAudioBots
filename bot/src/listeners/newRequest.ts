da6c580716cf9431c9e3e4f01ec508ef4a5b0aeb
choke-dev <admin@choke.dev>
2025-11-07 23:42:11 +0800

    aaaaa

--- bot/src/listeners/newRequest.ts
+++ bot/src/listeners/newRequest.ts
@@ -118,7 +118,6 @@ export class UserEvent extends Listener {
 
 			// Subscribe to the audioRequests channel
 			await this.subscriber.subscribe('autoAcceptWhitelistRequests', async (message) => {
-				console.log(message, { depth: null })
 				try {
 					const requestData = JSON.parse(message);
 					if (!requestData.requestId) return;
@@ -168,7 +167,7 @@ export class UserEvent extends Listener {
 								// Public message - must be in English
 								const doneText = [
 									`ℹ️ This request was automatically approved. (Moderation skipped)`,
-									`Submitter: [${requestData.requester.username}](https://www.roblox.com/users/${requestData.requester.id})`,
+									`Submitter: [${requestData.requester.roblox.username}](https://www.roblox.com/users/${requestData.requester.roblox.id})`,
 									...(requestData.audioVisibility === 'PRIVATE' ? [':lock: Marked as private — hidden from search results'] : []),
 									'',
 									'```',
